91905c18519425bf4e2ea8d8c17d7e790aaaaee2
idris demir <idris.dmr@hotmail.com>
2021-11-11 02:17:09 +0300

    sign_up edited

--- app/Views/sign_up.php
+++ app/Views/sign_up.php
@@ -1,57 +1,57 @@
-<?= view ('sections/header')?>
-<div class = "container" style="display:flex;height: min-content">
-    <div class="row align-items-center" style="display:flex;width= 50%;
-        border: 2px solid black;padding:10px 20px;">
+<?= view('sections/header') ?>
+<h1 class="article-title text-center background-success" style="margin:0 0 30px 0;padding: .80rem 0; ">Join Us</h1>
+<div class="container" style="display:flex;height: min-content">
+    <form action="save.php" method="POST" class="row" style="display:flex;padding:10px 20px;flex: 1;">
+    <div class="row" style="display:flex;padding:10px 20px;flex: 1;">
 
-            <div class="form-group col sm-4">
-                <label for = "name"><b>Name</b></label>
+            <div class="form-group sm-6" style="padding-left: 5px">
+                <label for="name"><b>Username</b></label>
                 <input type="text" placeholder="Enter Name" name="name" required>
             </div>
 
-            <div class="form-group col sm-4">
-                <label for="uname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="uname" required>
-            </div>
-
-            <div class="form-group col sm-4">
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" required>
-            </div>
-
-            <div class = "form-group col sm-4">
-                <label for = "surname"><b>Surname</b></label>
-                <input type="text" placeholder="Enter Surname" name="surname" required>
-            </div>
-
-            <div class="form-group col sm-4">
+            <div class="form-group sm-6" style="padding-left: 5px">
                 <label for="eMail"><b>Email</b></label>
                 <input type="email" placeholder="Enter Email" name="eMail" required>
             </div>
 
-            <div class="form-group col sm-4">
+            <div class="form-group sm-6" style="padding-left: 5px">
+                <label for="surname"><b>Name</b></label>
+                <input type="text" placeholder="Enter Surname" name="surname" required>
+            </div>
+
+            <div class="form-group sm-6" style="padding-left: 5px">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required>
+            </div>
+
+            <div class="form-group sm-6" style="padding-left: 5px">
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required>
+            </div>
+
+            <div class="form-group sm-6" style="padding-left: 5px">
                 <label for="c_psw"><b>Password</b></label>
                 <input type="password" placeholder="Enter Password Again" name="c_psw" required>
             </div>
 
-            <div class="form-inline" style="width: min-content;height: auto;margin: 0 auto;position:relative">
-                <fieldset class="form-group col sm-4 offset sm-4">
-                    <p>Gender</p>
-                    <label for="gender" class="paper-switch-tile">
-                        <input id="gender" name="gender" type="checkbox" />
-                        <div class="paper-switch-tile-card border">
-                            <div class="paper-switch-tile-card-front border">Male</div>
-                            <div class="paper-switch-tile-card-back border">Female</div>
-                        </div>
-                    </label>
-                </fieldset>
+            <div class="form-group sm-6" style="padding-left: 5px">
+                <label for="paperSwitch1" class="paper-switch-tile">
+                    Gender
+                    <input id="paperSwitch1" name="paperSwitch1" type="checkbox"/>
+                    <div class="paper-switch-tile-card border">
+                        <div class="paper-switch-tile-card-front border">Female</div>
+                        <div class="paper-switch-tile-card-back border background-primary">Male</div>
+                    </div>
+                </label>
             </div>
+            <div class="sm-6" style="display: flex;align-items: end;flex-direction: row-reverse;">
+                <button class="btn-outline-primary" type="submit">SignUp</button>
+            </div>
+    </div>
+    </form>
+    <div style="background: url('<?= base_url('img/img.png') ?>');flex: 1;background-size: contain;">
 
     </div>
-
 </div>
 
-<div style="width:min-content;margin: 0 auto; position: relative">
-    <button class="btn-outline-primary" type="submit">SignUp</button>
-</div>
-
-<?= view ('sections/footer')?>
+<?= view('sections/footer') ?>
