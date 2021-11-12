2ebb895648af509600c613c2812da34e92890d87
Batuhan <batuhanm35@gmail.com>
2021-11-12 17:05:50 +0300

    Remerging changes

--- app/Views/sign_up.php
+++ app/Views/sign_up.php
@@ -4,51 +4,35 @@
     <form action="../user/create" method="POST" class="row" style="display:flex;padding:10px 20px;flex: 1;">
     <div class="row" style="display:flex;padding:10px 20px;flex: 1;">
 
-            <div class="form-group sm-6" style="padding-left: 5px">
+        <div class="form-group sm-6" style="padding-left: 5px">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+        </div>
 
-                <label for="uname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="uname" required>
+        <div class="form-group sm-6" style="padding-left: 5px">
+            <label for="eMail"><b>Email</b></label>
+            <input type="email" placeholder="Enter Email" name="eMail" required>
+        </div>
 
-                <label for="usr_username"><b>Username</b></label>
-                <input type="text" placeholder="Enter Name" name="usr_username" required>
+        <div class="form-group sm-6" style="padding-left: 5px">
+            <label for="name"><b>Name</b></label>
+            <input type="text" placeholder="Enter Name" name="name" required>
+        </div>
 
-            </div>
+        <div class="form-group sm-6" style="padding-left: 5px">
+            <label for="surname"><b>Surname</b></label>
+            <input type="text" placeholder="Enter Surname" name="surname" required>
+        </div>
 
-            <div class="form-group sm-6" style="padding-left: 5px">
-                <label for="usr_mail"><b>Email</b></label>
-                <input type="email" placeholder="Enter Email" name="usr_mail" required>
-            </div>
+        <div class="form-group sm-6" style="padding-left: 5px">
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+        </div>
 
-            <div class="form-group sm-6" style="padding-left: 5px">
-
-                <label for="name"><b>Name</b></label>
-                <input type="text" placeholder="Enter Name" name="name" required>
-            </div>
-
-            <div class="form-group sm-6" style="padding-left: 5px">
-                <label for="surname"><b>Surname</b></label>
-                <input type="text" placeholder="Enter Surname" name="surname" required>
-
-                <label for="usr_name"><b>Name</b></label>
-                <input type="text" placeholder="Enter Surname" name="usr_name" required>
-            </div>
-
-            <div class="form-group sm-6" style="padding-left: 5px">
-                <label for="usr_surname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="usr_surname" required>
-
-            </div>
-
-            <div class="form-group sm-6" style="padding-left: 5px">
-                <label for="usr_password"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="usr_password" required>
-            </div>
-
-            <div class="form-group sm-6" style="padding-left: 5px">
-                <label for="usr_passwordAgain"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password Again" name="usr_passwordAgain" required>
-            </div>
-
+        <div class="form-group sm-6" style="padding-left: 5px">
+            <label for="c_psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password Again" name="c_psw" required>
+        </div>
             <div class="sm-6" style="display: flex;align-items: end;flex-direction: row-reverse;">
                 <button class="btn-outline-primary" type="submit">SignUp</button>
             </div>
