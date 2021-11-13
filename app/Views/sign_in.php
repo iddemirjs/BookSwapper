b733fe860568e88c8d3609b9acd7fe177d119cc7
HFurkanBirel <hidayetfurkanbirel@gmail.com>
2021-11-13 21:45:24 +0300

    Signin Alert added and conflict over.

--- app/Views/sign_in.php
+++ app/Views/sign_in.php
@@ -1,32 +1,37 @@
 <?= view('sections/header') ?>
 
-<div class = "container" style="display:flex;width: min-content; margin-bottom: 100px; margin-top: 100px; border: 2px solid black;">
-    <div class="row align-items-center" style="display:table-cell; margin-top: 0px; padding: 10px">
+<div class="container" style="display:flex;width: min-content; margin-bottom: 0px; margin-top: 0px;">
+    <div style="display:table-row; margin-top: 20px; padding: 100px">
         <form action="<?= base_url('user/login'); ?>" method="POST">
             <div class="form-group">
                 <div class="form-group" style="justify-content: center; display: flex; font-size: xxx-large">
                     <label><b>LOGIN</b></label>
                 </div>
-                <div class="form-group col sm-4">
-                    <label for="uname"><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="usr_username" style="width: fit-content" required>
+                <div class="form-group row flex-spaces">
+                    <div class="alert alert-danger">Your mother has been died because of colera</div>
                 </div>
-                <div class="form-group col sm-4">
+                <div class="form-group">
+                    <label for="uname"><b>Username</b></label>
+                    <input type="text" placeholder="Enter Username" name="uname" style="width: fit-content" required="">
+                </div>
+                <div class="form-group">
                     <label for="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="usr_password" style="width: fit-content" required>
+                    <input type="password" placeholder="Enter Password" name="psw" style="width: fit-content" required="">
+
                 </div>
             </div>
-            <div class = "form-group" style="display: flex; justify-content: center;">
+            <div class="form-group" style="display: flex; justify-content: center;">
                 <div style="display:flex;">
                     <input type="checkbox" name="paperChecks" id="paperChecks1" value="option 1" style="width: min-content">
-                    <label for="paperChecks1" style="width: fit-content"><span>Remember Me</span></label>
+                    <label for="paperChecks1" style="width: fit-content"><span>&nbsp;Remember Me</span></label>
                 </div>
             </div>
 
             <div style="display:flex;justify-content: center">
                 <button class="btn-outline-primary" type="submit">Login</button>
             </div>
-        </form>
+        </form>>
     </div>
 </div>
+
 <?= view('sections/footer'); ?>
