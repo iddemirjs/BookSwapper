1283b5fcedd6cf04e8a3cafe2c3aa4f6e2c327fb
Batuhan <batuhanm35@gmail.com>
2021-12-16 15:40:47 +0300

    Modal reconfigured.

--- app/Views/listbooks.php
+++ app/Views/listbooks.php
@@ -89,19 +89,24 @@
         <input class="modal-state" id="modal-1" type="checkbox">
         <div class="modal">
             <label class="modal-bg" for="modal-1"></label>
-            <div class="modal-body">
+            <div class="modal-body" style="max-width: 100%">
                 <label class="btn-close" for="modal-1">X</label>
-                <h3 class="modal-title">1984</h3>
-                <img src="https://onehundredonebooks.files.wordpress.com/2011/02/1984-book9.jpg" alt="1984"
-                     style="box-sizing: border-box">
-                <!-- Buraya kitabın küçük bir resmi eklenebilir. -->
-                <h5 id="author-name" class="modal-subtitle">Orwell, George</h5>
-                <p class="modal-text">Lorem Ipsum Dolor Sit Amet</p>
-                <label class="paper-btn" for="modal-1">Nice!</label>
+                <h3 class="modal-title">Book Information</h3>
+                <div class="row">
+                    <div class="form-group sm-6" style="width: fit-content">
+                        <!-- Buraya kitabın küçük bir resmi eklenebilir. -->
+                        <img src="https://onehundredonebooks.files.wordpress.com/2011/02/1984-book9.jpg" alt="1984"
+                             style="box-sizing: border-box;max-width: 100%">
+                    </div>
+                    <div class="form-group sm-6" style="width: max-content">
+                        <h3 class="modal-title">1984</h3>
+                        <h5 id="author-name" class="modal-subtitle">Orwell, George</h5>
+                        <p class="modal-text">Lorem Ipsum Dolor Sit Amet </p>
+                    </div>
+                </div>
+                <label class="paper-btn" for="modal-1">Go to listing details</label>
             </div>
         </div>
-
-
     </div>
 </div>
 <?= view('sections/footer'); ?>
