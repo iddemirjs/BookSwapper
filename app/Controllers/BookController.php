52fbc597cf088b4dfc4308333dfb08b4d1fee45f
Batuhan <batuhanm35@gmail.com>
2021-12-21 00:36:27 +0300

    Ordering by categories added for books

--- app/Controllers/BookController.php
+++ app/Controllers/BookController.php
@@ -9,12 +9,24 @@ class BookController extends BaseController
     public function index()
     {
 
-        $results_per_page = 1;
+
+        $authors = model('AuthorModel')->findAll();
+        $categories = model('CategoryModel')->findAll();
+        $results_per_page = 10;
         $book_model = model('BookModel');
-        $page_books = $book_model->paginate($results_per_page);
+        $page_books['books'] = $book_model->paginate($results_per_page);
+        for($i = 0; $i<count($page_books['books']); $i++)
+        {
+            $id = $page_books['books'][$i]->bk_id;
+            $page_books['books_categories'][$i] = $this->get_categories($id);
+        }
+
         return view('listbooks', [
-            'books' =>  $page_books,
-            'pager'=>$book_model->pager
+            'books' =>  $page_books['books'],
+            'pager'=> $book_model->pager,
+            'authors' => $authors,
+            'categories' => $categories,
+            'books_categories' => $page_books['books_categories']
         ]);
     }
     public function view_details($bookId)
@@ -25,6 +37,49 @@ class BookController extends BaseController
         return view ('bookdetails',
             [ 'book'=>$book
             ]);
+    }
+    public function get_categories($bookId)
+    {
+        $db = db_connect();
+        $categories = $db->query("SELECT cat_id,cat_name FROM tbl_bookcategory,tbl_category 
+                        WHERE bc_bookId = $bookId AND cat_id = bc_catId" )->getResult();
+        return $categories;
+    }
+    public function sort_by_category($categoryId)
+    {
+        $db = db_connect();
+        $results_per_page = 10;
+        $book_model = new BookModel();
+        /*
+        $query = $db ->query("SELECT *  FROM tbl_book as book,tbl_bookcategory,tbl_category
+                         WHERE bk_id = bc_bookId AND cat_id = bc_catId AND cat_id = $categoryId")->getResult();
+        */
+
+
+        $where_key = 'bk_id = bc_bookId AND cat_id = bc_catId AND cat_id = ' . $categoryId;
+        $page_books['books'] = $book_model->select('bk_id,bk_ownerId,
+                                    bk_title,bk_authorId,bk_description,bk_editionNumber,bk_mainImgUrl')
+            ->from('tbl_bookcategory')
+            ->from('tbl_category')
+            ->where($where_key)
+            ->paginate($results_per_page);
+        $authors = model('AuthorModel')->findAll();
+        $categories = model('CategoryModel')->findAll();
+        for($i = 0; $i<count($page_books['books']); $i++)
+        {
+            $id = $page_books['books'][$i]->bk_id;
+            $page_books['books_categories'][$i] = $this->get_categories($id);
+        }
+
+
+        return view('listbooks', [
+            'books' =>  $page_books['books'],
+            'pager'=> $book_model->pager,
+            'authors' => $authors,
+            'categories' => $categories,
+            'books_categories' => $page_books['books_categories']
+        ]);
+
     }
 
 }
--- app/Views/listbooks.php
+++ app/Views/listbooks.php
@@ -12,24 +12,24 @@
     <ul class="sort-options" style="display: inline">
         <paper-dropdown-menu id="sort-author" label="Author" style="height: min-content">
             <paper-listbox slot="dropdown-content" selected="0">
-                <paper-item>Author1</paper-item>
-                <paper-item>Author2</paper-item>
-                <paper-item>Author3</paper-item>
-                <paper-item>Author4</paper-item>
+                <?php foreach ($authors as $key => $author):?>
+                <paper-item><?=$author->auth_name; ?> <?=$author->auth_surname; ?></paper-item>
+                <?php endforeach ?>
             </paper-listbox>
         </paper-dropdown-menu>
-        <paper-dropdown-menu id="sort-genre" label="Genre" style="height: min-content">
+        <paper-dropdown-menu id="sort-category" label="Category" style="height: min-content">
             <paper-listbox slot="dropdown-content" selected="0">
-                <paper-item>Genre1</paper-item>
-                <paper-item>Genre2</paper-item>
-                <paper-item>Genre3</paper-item>
-                <paper-item>Genre4</paper-item>
+                <?php foreach ($categories as $key => $category):?>
+                <a href="/bookcontroller/sort_by_category/<?=$category->cat_id;?>"
+                <paper-item><?=$category->cat_name; ?></paper-item>
+                <?php endforeach ?>
             </paper-listbox>
         </paper-dropdown-menu>
     </ul>
 </div>
 <div class="container" id="booklistcontainer">
     <div class="booklist">
+        <?php $i = 0 ?>
         <?php foreach ($books as $key => $book): ?>
         <a href="/bookcontroller/view_details/<?= $book->bk_id; ?>">
             <btn class="paper-btn margin"
@@ -38,6 +38,14 @@
                 <p class="book-title"><?= $book->bk_title; ?></p>
                 <span class="book-author">Orwell, George</span>
                 <span class="book-edition-number"><?= $book->bk_editionNumber; ?></span>
+                <span class="book-category">
+
+                <?php foreach ($books_categories[$i] as $key => $books_category): ?>
+                    <?= $books_category->cat_name; ?>
+                <?php endforeach ?>
+
+                </span>
+                <?php $i = $i + 1?>
             </btn>
         </a>
         <?php endforeach ?>
