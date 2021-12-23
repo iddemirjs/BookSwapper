4942f9525f5ec4f5e7cc369075226690e9aac6f1
HFurkanBirel <hidayetfurkanbirel@gmail.com>
2021-12-23 16:51:01 +0300

    Image upload added into the register.(Need some arrangement)

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -65,4 +65,47 @@ class User extends BaseController {
         // Display the alert box
         echo "<script>alert('$all_errors');</script>";
     }
+
+    public function upload()
+    {
+        helper(['form', 'url']);
+
+        $database = db_connect();
+        // Tablo ilişkisi düzeltilecek
+        $builder = $database->table('users');
+
+        $validateImage = $this->validate([
+            'file' => [
+                'uploaded[file]',
+                'mime_in[file, image/png, image/jpg,image/jpeg, image/gif]',
+                'max_size[file, 4096]',
+            ],
+        ]);
+
+        $response = [
+            'success' => false,
+            'data' => '',
+            'msg' => "Image could not upload"
+        ];
+
+        if ($validateImage) {
+            $imageFile = $this->request->getFile('file');
+            $imageFile->move(WRITEPATH . 'uploads');
+
+            $data = [
+                'img_name' => $imageFile->getClientName(),
+                'file'  => $imageFile->getClientMimeType()
+            ];
+
+            $save = $builder->insert($data);
+
+            $response = [
+                'success' => true,
+                'data' => $save,
+                'msg' => "Image successfully uploaded"
+            ];
+        }
+
+        return $this->response->setJSON($response);
+    }
 }
