86e1c5ef018a7f905da7a0def55d985babdc0664
idris demir <idris.dmr@hotmail.com>
2022-01-02 20:21:32 +0300

    make offer added.

--- app/Views/make_offer.php
+++ app/Views/make_offer.php
@@ -1,80 +1,58 @@
 <?= view('sections/header') ?>
-<form action="#" style="display: contents">
+<form action="/offer/create" method="post" style="display: contents">
 
     <div class="row flex-center">
         <div class="col-4">
             <div class="card-header" style="display: flex;justify-content: space-around;padding: 5px">
-                <div style="line-height: 50px;">Your Side</div>
-                <div style="float: right"><img src="https://avatars.githubusercontent.com/u/28766071?v=4"
-                                               alt="Random Unsplash" style="height: 50px;"></div>
+                <div style="line-height: 50px;">Your Side
+                    : <?= $offerUser->usr_name . ' ' . $offerUser->usr_surname; ?></div>
+                <div style="float: right">
+                    <img src="/uploads/user_images/<?= $offerUser->usr_img_url; ?>"
+                         alt="Random Unsplash" style="height: 50px;">
+                </div>
+                <input name="of_creatorUserId" type="hidden" value="<?= $offerUser->usr_id; ?>">
             </div>
             <div class="card-body">
                 <h4 class="card-title">Books to be given</h4>
                 <ul style="width: 100%;list-style: none;min-height: 200px;">
-                    <li class=" d-flex" style="padding: 3px 5px;margin-top: 5px;">
-                        <div style="width: calc( 100% - 50px)">
-                            <h6>bunu ekledin</h6>
-                            <span class="border border-2 border-primary" style="padding: 2px 5px;">Edition : 6</span>
-                            <span class="border border-2 border-primary" style="padding: 2px 5px;">Sil</span>
-                        </div>
-                        <img src="https://unsplash.it/900" alt="Random Unsplash" style="height: 50px;">
-                    </li>
-                    <li class=" d-flex" style="padding: 3px 5px;margin-top: 5px;">
-                        <div style="width: calc( 100% - 50px)">
-                            <h6>bunu ekledin</h6>
-                            <span class="border border-2 border-primary" style="padding: 2px 5px;">Edition : 6</span>
-                            <span class="border border-2 border-primary" style="padding: 2px 5px;">Sil</span>
-                        </div>
-                        <img src="https://unsplash.it/900" alt="Random Unsplash" style="height: 50px;">
-                    </li>
-                    <li class=" d-flex" style="padding: 3px 5px;margin-top: 5px;">
-                        <div style="width: calc( 100% - 50px)">
-                            <h6>bunu ekledin</h6>
-                            <span class="border border-2 border-primary" style="padding: 2px 5px;">Edition : 6</span>
-                            <span class="border border-2 border-primary" style="padding: 2px 5px;">Sil</span>
-                        </div>
-                        <img src="https://unsplash.it/900" alt="Random Unsplash" style="height: 50px;">
-                    </li>
+
                 </ul>
                 <div class="form-inline" style="display: flex;justify-items: end;">
                     <select name="" id="" style="width: calc( 100% - 50px);">
-                        <option value="afs">asfafs</option>
+                        <?php foreach ($offerBooks as $key => $book): ?>
+                            <option value="<?= $book->bk_id; ?>"><?= $book->bk_title . ' - EN : ' . $book->bk_editionNumber; ?></option>
+                        <?php endforeach ?>
                     </select>
-                    <span class="alert alert-success" style="width: 50px;height: 50px;margin-bottom: 0px;">+</span>
+                    <span class="alert alert-success addBookToList"
+                          style="width: 50px;height: 50px;margin-bottom: 0px;">+</span>
                 </div>
             </div>
         </div>
         <div class="col-4">
             <div class="card-header" style="display: flex;justify-content: space-around; padding: 5px;">
-                <div style="line-height: 50px;">Target User</div>
-                <div style="float: right"><img src="https://unsplash.it/900" alt="Random Unsplash"
-                                               style="height: 50px;"></div>
+                <div style="line-height: 50px;">Target User :
+                    : <?= $targetUser->usr_name . ' ' . $targetUser->usr_surname; ?></div>
+                <div style="float: right">
+                    <img src="/uploads/user_images/<?= $targetUser->usr_img_url; ?>"
+                         alt="Random Unsplash"
+                         style="height: 50px;">
+                </div>
+                <input name="of_targetUserId" type="hidden" value="<?= $targetUser->usr_id; ?>">
+
             </div>
             <div class="card-body">
                 <h4 class="card-title">Books to want</h4>
                 <div class="form-inline" style="display: flex;">
                     <ul style="width: 100%;list-style: none;min-height: 200px;">
-                        <li class=" d-flex" style="padding: 3px 5px;margin-top: 5px;">
-                            <div style="width: calc( 100% - 50px)">
-                                <h6>bunu ekledin</h6>
-                                <span class="border border-2 border-primary" style="padding: 2px 5px;">Edition : 6</span>
-                                <span class="border border-2 border-primary" style="padding: 2px 5px;">Sil</span>
-                            </div>
-                            <img src="https://unsplash.it/900" alt="Random Unsplash" style="height: 50px;">
-                        </li>
-                        <li class=" d-flex" style="padding: 3px 5px;margin-top: 5px;">
-                            <div style="width: calc( 100% - 50px)">
-                                <h6>bunu ekledin</h6>
-                                <span class="border border-2 border-primary" style="padding: 2px 5px;">Edition : 6</span>
-                                <span class="border border-2 border-primary" style="padding: 2px 5px;">Sil</span>
-                            </div>
-                            <img src="https://unsplash.it/900" alt="Random Unsplash" style="height: 50px;">
-                        </li>
+
                     </ul>
                     <select name="" id="" style="width: calc( 100% - 50px);">
-                        <option value="afs">asfafs</option>
+                        <?php foreach ($targetUserBooks as $key => $book): ?>
+                            <option value="<?= $book->bk_id; ?>"><?= $book->bk_title . ' - EN : ' . $book->bk_editionNumber; ?></option>
+                        <?php endforeach ?>
                     </select>
-                    <span class="alert alert-success" style="width: 50px;height: 50px;margin-bottom: 0px;">+</span>
+                    <span class="alert alert-success addBookToList"
+                          style="width: 50px;height: 50px;margin-bottom: 0px;">+</span>
                 </div>
             </div>
         </div>
@@ -82,15 +60,19 @@
     <div class="row flex-center" style="padding: ">
         <div class="form-group col-8">
             <label for="">Description:</label>
-            <textarea class="col-8" name="" style="width: 100%;" id="" cols="30" rows="5"></textarea>
+            <textarea class="col-8" name="of_description" style="width: 100%;" id="" cols="30" rows="5"></textarea>
         </div>
     </div>
     <div class="row">
         <div class="col-10">
-            <button class="btn-warning" style="float: right;">Lets Change</button>
+            <button class="btn-warning" type="submit" style="float: right;">Lets Change</button>
         </div>
     </div>
 </form>
 
-<?= view('sections/footer'); ?>
+<?= view('sections/footer', ['scripts' => [
+    'lister.js',
+    "../libs/misc/datatables/datatables.min.css",
+    "../libs/misc/datatables/datatables.min.js"
+]]); ?>
 
