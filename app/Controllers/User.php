6975318ce96db66dead1cc9c62f77de0bbb8d1ba
HFurkanBirel <hidayetfurkanbirel@gmail.com>
2022-01-03 18:51:25 +0300

    Little fix.

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -160,6 +160,53 @@ class User extends BaseController
         }
     }
 
+
+    public function go_update_user(){
+        $user = session()->get('user');
+        return view('user_edit', ['user'=>$user]);
+    }
+
+    public function update_user($id)
+    {
+        define ('img_upload_dir', realpath(dirname('user_images')));
+        $validation = \Config\Services::validation();
+
+        if ($validation->run($this->request->getPost(), 'validUserNew')) {
+            $data = $this->request->getPost();
+            $userModel = new UserModel();
+            $user = new \App\Entities\User();
+            $user->fill($data);
+
+            $imageName = time() . $_FILES['usr_img_url']['name'];
+            $target =  img_upload_dir . '\uploads\user_images\\' . $imageName;
+
+            $user->usr_img_url = $imageName;
+
+            move_uploaded_file($_FILES['usr_img_url']['tmp_name'], $target);
+            $userModel->update($id, $user);
+
+            $tmp = session()->get('user');
+
+
+            session()->set('user', [
+                'usr_username' => $user->usr_username,
+                'usr_name' => $user->usr_name,
+                'usr_surname' => $user->usr_surname,
+                'usr_mail' => $user->usr_mail,
+                'usr_img_url' => $user->usr_img_url,
+                'usr_id' => $tmp['usr_id'],
+                'usr_type' => $tmp['usr_type'],
+            ]);
+
+
+            return redirect()->to(base_url('user/go_update_user'));
+        } else {
+            $this->alert_function($validation->getErrors());
+            return redirect()->to(base_url('user/go_update_user'));
+        }
+    }
+
+
     public function login()
     {
         $validation = \Config\Services::validation();
